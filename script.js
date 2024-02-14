7ad6b7ea69e442d5aba2fffe2dacc5134c622992
Farhan <153175749+Far34n@users.noreply.github.com>
2024-02-14 17:49:25 +0330

    with function play round structer and role of game created

--- script.js
+++ script.js
@@ -2,3 +2,40 @@ function getComputerChoice(choices) {
     let index = Math.floor(Math.random() * choices.length);
     return choices[index]
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+    if (computerSelection === 'rock') {
+        if (playerSelection === 'rock') {
+            return '='
+        } else if (playerSelection === 'paper') {
+            return 'you win'
+        } else if (playerSelection === 'scissors') {
+            return 'you lose'
+        }
+    } else if (computerSelection == 'paper') {
+        if (playerSelection === 'rock') {
+            return 'you lose'
+        } else if (playerSelection === 'paper') {
+            return '='
+        } else if (playerSelection === 'scissors') {
+            return 'you win'
+        }
+    } else if (computerSelection === 'scissors') {
+        if (playerSelection === 'rock') {
+            return 'you win'
+        } else if (playerSelection === 'paper') {
+            return 'you lose'
+        } else if (playerSelection === 'scissors') {
+            return '='
+        }
+    }
+
+
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice(['rock', 'paper', 'scissors']);
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection));
