914b0fd148b0891165c99354f8a2e04b990ab825
Far34n <153175749+Far34n@users.noreply.github.com>
2024-02-16 21:51:12 +0330

    created playGame() function for play five round of game and keep score ans reports a winner or losser at the end

--- script.js
+++ script.js
@@ -8,34 +8,59 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerSelection.toLowerCase()
     if (computerSelection === 'rock') {
         if (playerSelection === 'rock') {
-            return '='
+            return 0
         } else if (playerSelection === 'paper') {
-            return 'you win'
+            return 1
         } else if (playerSelection === 'scissors') {
-            return 'you lose'
+            return -1
         }
     } else if (computerSelection == 'paper') {
         if (playerSelection === 'rock') {
-            return 'you lose'
+            return -1
         } else if (playerSelection === 'paper') {
-            return '='
+            return 0
         } else if (playerSelection === 'scissors') {
-            return 'you win'
+            return 1
         }
     } else if (computerSelection === 'scissors') {
         if (playerSelection === 'rock') {
-            return 'you win'
+            return 1
         } else if (playerSelection === 'paper') {
-            return 'you lose'
+            return -1
         } else if (playerSelection === 'scissors') {
-            return '='
+            return 0
         }
     }
 
 
 }
+function playGame() {
+    let computerScore = 0;
+    let playerScore = 0;
+    let playerSelection = ''
+    let computerSelection = ''
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice(['rock', 'paper', 'scissors']);
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection));
+    for (let count = 1; count <= 5; count++) {
+        playerSelection = prompt(`select rock,paper or scissors \nRound : ${count} of 5 \n your_score : ${playerScore} \ncomputer_score : ${computerScore}`)
+        computerSelection = getComputerChoice(['rock', 'paper', 'scissors']);
+        let tempScore = playRound(playerSelection, computerSelection)
+        if (tempScore === 1) {
+            playerScore += 1;
+            alert(`You won round ${count}  \n you_select : ${playerSelection} \ncomputer_select : ${computerSelection}`)
+        } else if (tempScore === -1) {
+            computerScore += 1;
+            alert(`You losed round ${count} \n you_select : ${playerSelection} \ncomputer_select : ${computerSelection} `)
+        } else {
+            alert(`the same select : ${playerSelection}`)
+        }
+    }
+    if (playerScore > computerScore) {
+        alert(`You're winner \nyour_score : ${playerScore}\ncomputer_score : ${computerScore}`)
+    } else if (playerScore < computerScore) {
+        alert(`You're loser \nyour_score : ${playerScore}\ncomputer_score : ${computerScore}`)
+    } else {
+        alert("the score is same :  ${playerScore} ")
+    }
+}
+
+playGame()
